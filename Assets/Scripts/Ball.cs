14b3968bd66b5cd7d26714b29fec524bbe9f330a
Kirill <podgol04@gmail.com>
2024-12-29 20:26:25 +0300

    реализация прыжка, с добавление выбора слоя земли

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,22 +1,35 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Ball : MonoBehaviour
 {
     private InputAsset _input;
     private Rigidbody _rb;
-
     [SerializeField] private float moveSpeed = 5f; // Скорость движения
     [SerializeField] private float gravity = -9.81f; // Сила гравитации
+    [SerializeField] private float jumpForce = 5f; // Силла прыжка
     [SerializeField] private float rotationSpeed = 10f; // Скорость вращения шарика
-
+    [SerializeField] private LayerMask groundLayer; // Слой для земли
+    [SerializeField] private float groundCheckDistance = 1f; // Дистанция проверки земли
     private Vector3 _moveDirection;
     private CameraFollow _cameraFollow;
+    private bool _isGrounded;
+
 
     private void Awake()
     {
         _input = new InputAsset();
         _rb = GetComponent<Rigidbody>();
-        if (Camera.main != null) _cameraFollow = Camera.main.GetComponent<CameraFollow>();
+        if (Camera.main != null)
+        {
+            _cameraFollow = Camera.main.GetComponent<CameraFollow>();
+        }
+        else
+        {
+            Debug.LogError("Main Camera not found! Please assign a camera with the CameraFollow script.");
+            enabled = false; 
+        }
+
     }
 
     private void OnEnable()
@@ -24,31 +37,41 @@ public class Ball : MonoBehaviour
         _input.Gameplay.Enable();
         _input.Gameplay.Move.performed += OnMovePerformed;
         _input.Gameplay.Move.canceled += OnMoveCanceled;
+        _input.Gameplay.Jump.performed += JumpOnperformed;
+        _input.Gameplay.Jump.canceled += JumpOnperformed;
     }
-
+    
     private void OnDisable()
     {
         _input.Gameplay.Move.performed -= OnMovePerformed;
         _input.Gameplay.Move.canceled -= OnMoveCanceled;
+        _input.Gameplay.Jump.performed -= JumpOnperformed;
+        _input.Gameplay.Jump.canceled -= JumpOnperformed;
         _input.Gameplay.Disable();
     }
 
-    private void OnMovePerformed(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    private void OnMovePerformed(InputAction.CallbackContext context)
     {
         Vector2 inputVector = context.ReadValue<Vector2>();
         _moveDirection = new Vector3(inputVector.x, 0, inputVector.y);
     }
 
-    private void OnMoveCanceled(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    private void OnMoveCanceled(InputAction.CallbackContext context)
     {
         _moveDirection = Vector3.zero;
     }
-
+    
+    private void JumpOnperformed(InputAction.CallbackContext obj)
+    {
+        Debug.Log("Jump key pressed");
+        if (_isGrounded)
+        {
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
     private void FixedUpdate()
     {
-        if (_cameraFollow == null) return;
-
-        // Преобразование направления движения в глобальные координаты
+        CheckGround();
         Vector3 forward = _cameraFollow.GetCameraRotation() * Vector3.forward;
         Vector3 right = _cameraFollow.GetCameraRotation() * Vector3.right;
 
@@ -67,5 +90,14 @@ public class Ball : MonoBehaviour
             Vector3 torque = new Vector3(globalMoveDirection.z, 0, -globalMoveDirection.x) * rotationSpeed;
             _rb.AddTorque(torque, ForceMode.Acceleration);
         }
+    }
+    private void CheckGround()
+    {
+        RaycastHit hit;
+        // Проверяем наличие земли под объектом с использованием Raycast
+        _isGrounded = Physics.Raycast(transform.position, Vector3.down, out hit, groundCheckDistance, groundLayer);
+
+        // Для отладки можно добавить линию визуализации
+        Debug.DrawRay(transform.position, Vector3.down * groundCheckDistance, _isGrounded ? Color.green : Color.red);
     }
 }
--- Assets/Scripts/InputAsset.cs
+++ Assets/Scripts/InputAsset.cs
@@ -44,6 +44,15 @@ public partial class @InputAsset: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": true
+                },
+                {
+                    ""name"": ""Jump"",
+                    ""type"": ""Button"",
+                    ""id"": ""15842962-64a6-4070-8181-085f58e62c41"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -112,6 +121,17 @@ public partial class @InputAsset: IInputActionCollection2, IDisposable
                     ""action"": ""Look"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""34caf6a4-d0d6-4b8c-8016-1261c412699c"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Jump"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         },
@@ -150,6 +170,7 @@ public partial class @InputAsset: IInputActionCollection2, IDisposable
         m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
         m_Gameplay_Move = m_Gameplay.FindAction("Move", throwIfNotFound: true);
         m_Gameplay_Look = m_Gameplay.FindAction("Look", throwIfNotFound: true);
+        m_Gameplay_Jump = m_Gameplay.FindAction("Jump", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Pause = m_UI.FindAction("Pause", throwIfNotFound: true);
@@ -222,12 +243,14 @@ public partial class @InputAsset: IInputActionCollection2, IDisposable
     private List<IGameplayActions> m_GameplayActionsCallbackInterfaces = new List<IGameplayActions>();
     private readonly InputAction m_Gameplay_Move;
     private readonly InputAction m_Gameplay_Look;
+    private readonly InputAction m_Gameplay_Jump;
     public struct GameplayActions
     {
         private @InputAsset m_Wrapper;
         public GameplayActions(@InputAsset wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Gameplay_Move;
         public InputAction @Look => m_Wrapper.m_Gameplay_Look;
+        public InputAction @Jump => m_Wrapper.m_Gameplay_Jump;
         public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -243,6 +266,9 @@ public partial class @InputAsset: IInputActionCollection2, IDisposable
             @Look.started += instance.OnLook;
             @Look.performed += instance.OnLook;
             @Look.canceled += instance.OnLook;
+            @Jump.started += instance.OnJump;
+            @Jump.performed += instance.OnJump;
+            @Jump.canceled += instance.OnJump;
         }
 
         private void UnregisterCallbacks(IGameplayActions instance)
@@ -253,6 +279,9 @@ public partial class @InputAsset: IInputActionCollection2, IDisposable
             @Look.started -= instance.OnLook;
             @Look.performed -= instance.OnLook;
             @Look.canceled -= instance.OnLook;
+            @Jump.started -= instance.OnJump;
+            @Jump.performed -= instance.OnJump;
+            @Jump.canceled -= instance.OnJump;
         }
 
         public void RemoveCallbacks(IGameplayActions instance)
@@ -320,6 +349,7 @@ public partial class @InputAsset: IInputActionCollection2, IDisposable
     {
         void OnMove(InputAction.CallbackContext context);
         void OnLook(InputAction.CallbackContext context);
+        void OnJump(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
